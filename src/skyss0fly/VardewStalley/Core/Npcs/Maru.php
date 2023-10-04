beee2266b061bfdaf36e36ed9a43989fbef951ae
skyss0fly-pm-pl <144737802+skyss0fly-pm-pl@users.noreply.github.com>
2023-10-04 12:47:48 +1000

    Create Maru.php

---
+++ src/skyss0fly/VardewStalley/Core/Npcs/Maru.php
@@ -0,0 +1,18 @@
+<?php
+namespace skyss0fly\VardewStalley;
+use pocketmine\player\Player;
+
+final class Maru {
+  public Player = $player
+public function sendDialogue(): string 
+  {
+    if ($player instanceof Player) {
+$this->sendMessage($dialogue, $player);
+  }
+    else
+    {}
+  }
+public function likes() {
+  
+}
+}
